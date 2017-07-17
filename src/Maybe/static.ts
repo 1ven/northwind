9a095079ce8c994586226804986bcdd81b2eb4b7
1ven <1ven.mironchik@gmail.com>
2017-07-17 12:41:29 +0300

    Remove static

--- src/Maybe/static.ts
+++
@@ -1,8 +0,0 @@
-import Nothing from "./Nothing";
-import Just from "./Just";
-
-export const empty = () => new Nothing();
-
-export const zero = () => new Nothing();
-
-export const of = <T>(value: T) => new Just(value);
