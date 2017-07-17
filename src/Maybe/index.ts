841329640780b0f66ac79b8b31a1260ec6b52e64
1ven <1ven.mironchik@gmail.com>
2017-07-17 10:45:36 +0300

    Add null check

--- src/Maybe/index.ts
+++ src/Maybe/index.ts
@@ -14,9 +14,8 @@ export interface MaybeConstructor extends F.MonadConstructor, F.MonoidConstructo
 export const fromMaybe = <T>(initial: T, m: Maybe<T>): T =>
   isNothing(m) ? initial : m.__value;
 
-// TODO: check on null as well
 export const toMaybe = <T>(v?: T): Maybe<T> =>
-  typeof v === "undefined" ? new Nothing() : new Just(v);
+  typeof v === "undefined" || v === null ? new Nothing() : new Just(v);
 
 export { default as Just, isJust } from "./Just";
 export { default as Nothing, isNothing } from "./Nothing";
