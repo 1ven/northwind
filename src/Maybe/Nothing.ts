a5d0d6f23e7adadbf7a375a1e813d9689c823f8e
1ven <1ven.mironchik@gmail.com>
2017-07-17 12:41:55 +0300

    Fix statics. Add generics

--- src/Maybe/Nothing.ts
+++ src/Maybe/Nothing.ts
@@ -1,56 +1,54 @@
-import * as Z from "sanctuary-type-classes";
 import * as F from "fantasy-types";
 import { Maybe, MaybeConstructor } from "./";
-import { of, zero, empty } from "./static";
 
 interface Constructor extends MaybeConstructor { new () }
 
-const Nothing = <Constructor>class Nothing<T> implements Maybe<T> {
-  static of = of;
-  static zero = zero;
-  static empty = empty;
+const Nothing = <Constructor>class Nothing implements Maybe<void> {
+  static zero = () => new Nothing();
+  static empty = () => new Nothing();
+  static of = <T>(value: T) => new Nothing();
 
-  public equals(b: Maybe<T>) {
+  public equals<T1>(b: Maybe<T1>) {
     return isNothing(b);
   }
 
-  public lte(b: Maybe<T>) {
+  public lte<T1>(b: Maybe<T1>) {
     return true;
   }
 
-  public concat(b: Maybe<T>) {
+  public concat<T1>(b: Maybe<T1>) {
     return b;
   }
 
-  public map<T2>(f: (a: T) => T2) {
-    return this as any;
+  public map<T, T1>(f: (a: T) => T1) {
+    return this;
   }
 
-  public ap<T1>(b: Maybe<(x: T) => T1>) {
-    return this as any;
+  public ap<T, T1>(b: Maybe<(x: T) => T1>) {
+    return this;
   }
 
-  public chain<T1>(f: (a: T) => Maybe<T1>) {
-    return this as any;
+  public chain<T, T1>(f: (a: T) => Maybe<T1>) {
+    return this;
   }
 
   public alt<T1>(b: Maybe<T1>) {
     return b;
   }
 
-  public reduce<T1>(fn: (acc: T1, value: T) => T1, initial: T1) {
+  public reduce<T, T1>(fn: (acc: T1, value: T) => T1, initial: T1) {
     return initial;
   }
 
-  public traverse<T1>(
+  public traverse<T, T1>(
     A: F.ApplicativeConstructor,
     f: (a: T) => F.Applicative<T1>
   ) {
-    return Z.of(A, this);
+    return A.of(new Nothing());
   }
 
-  public extend<T1>(f: (a: Maybe<T>) => T1) {
-    return this as any;
+  public extend<T1>(f: (a: Nothing) => T1) {
+    return this;
   }
 };
 
