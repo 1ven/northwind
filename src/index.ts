0a46eeb35556c4d5b627fa70e875d0376ecf75f2
1ven <1ven.mironchik@gmail.com>
2017-07-31 19:42:06 +0300

    Upd

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,9 @@ import * as M from "./Maybe";
 
 export const Maybe = M;
 export type Maybe<T> = M.Maybe<T>;
+
+declare module "icecrown/lib/types/Functor" {
+  export interface MapFunction {
+    <T, T1>(f: (x: T) => T1, a: Maybe<T>): Maybe<T1>;
+  }
+}
